5479e241a843bd510ffdff834a4f525b3db6ab28
Kyle Murray <kyle.w.murray@outlook.com>
2026-04-08 22:39:53 +0100

    add documentation comments to models

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -1,27 +1,41 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace AppointmentManagementAPI{
-     public enum AppointmentStatus
-    {
-        Completed,
-        Pending,
-        Cancelled,
-        EnteredInError
-    }    
-
-
-
-
+    /// <summary>
+    /// Object that represents appointments and contains all information needed to manage them.
+    /// </summary>
     public class Appointment
     {
+        /// <summary>
+        /// Appointment unique ID. Automatically set with no user input.
+        /// </summary>
         public int Id {get; set;}
+        /// <summary>
+        /// Patients name. Required field and is initalised empty.
+        /// </summary>
         [Required]
         public string Patient {get; set;} = string.Empty;
+
+        /// <summary>
+        /// Department appointment is assigned to. Required field and is initalised empty.
+        /// </summary>
         [Required]
         public string Department {get; set;} = string.Empty;
+
+        /// <summary>
+        /// Clinician assigned to appointments name. Required field and is initalised empty.
+        /// </summary>
         [Required]
         public string Clinician {get; set;} = string.Empty;
+
+        /// <summary>
+        /// Whether appointment is Pending, Completed, Cancelled or Entered in error
+        /// </summary>
         public AppointmentStatus Status {get; set;}
+
+        /// <summary>
+        /// Time and date of appointment
+        /// </summary>
         [Required]
         public DateTime AppointmentTime {get; set;}
 
--- Models/CreateAppointmentDTO.cs
+++ Models/CreateAppointmentDTO.cs
@@ -1,9 +1,21 @@
 namespace AppointmentManagementAPI
 {
+    /// <summary>
+    /// Represents data needed to create an appointment
+    /// </summary>
     public class CreateAppointmentDTO
     {
+        /// <summary>
+        /// Patient name
+        /// </summary>
         public string? Patient {get; set;}
+        /// <summary>
+        /// Department appoint sits with
+        /// </summary>
         public string? Department {get; set;}
+        /// <summary>
+        /// Clinician patient will be seeing
+        /// </summary>
         public string? Clinician {get; set;}
         public DateTime AppointmentTime{get; set;}
     }
